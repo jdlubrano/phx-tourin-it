e20d02f257d8c66e7a7ec92a720242f935bd55ee
Joel Lubrano <joel.lubrano@gmail.com>
2025-04-17 21:29:12 -0400

    Add seeds to generate admin login url

--- priv/repo/seeds.exs
+++ priv/repo/seeds.exs
@@ -9,3 +9,20 @@
 #
 # We recommend using the bang functions (`insert!`, `update!`
 # and so on) as they will fail if something goes wrong.
+
+defmodule TourinIt.Seeds do
+  def generate_access_token(admin) when is_binary(admin) do
+    {:ok, admin} = TourinIt.Accounts.create_admin(%{username: admin})
+    generate_access_token(admin)
+  end
+
+  def generate_access_token(admin = %TourinIt.Accounts.User{}) do
+    TourinIt.Accounts.generate_user_access_token(admin)
+  end
+end
+
+admin_username = "admin"
+admin = TourinIt.Accounts.get_user_by_username(admin_username)
+access_token = TourinIt.Seeds.generate_access_token(admin || admin_username)
+
+IO.puts "Login as #{admin_username} at http://localhost:4000/organize/tours?token=#{access_token}"
