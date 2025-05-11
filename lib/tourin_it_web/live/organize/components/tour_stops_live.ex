cbe0b74a7a67246c57128cc3bec0264dabdb2407
Joel Lubrano <joel.lubrano@gmail.com>
2025-05-11 08:49:29 -0400

    Deleting tour stops

--- lib/tourin_it_web/live/organize/components/tour_stops_live.ex
+++ lib/tourin_it_web/live/organize/components/tour_stops_live.ex
@@ -27,6 +27,16 @@ defmodule TourinItWeb.Organize.Components.TourStopsLive do
     {:noreply, assign(socket, :new_tour_stop, nil)}
   end
 
+  def handle_event("delete", %{"id" => id}, socket) do
+    case Integer.parse(id) do
+      {int_id, _} ->
+        Repo.delete(%TourStop{id: int_id})
+        {:noreply, reload_tour_session(socket)}
+
+      :error -> {:noreply, socket}
+    end
+  end
+
   def handle_event("save", %{"tour_stop" => %{"id" => ""} = tour_stop_params}, socket) do
     create_params = Map.put(tour_stop_params, "tour_session_id", socket.assigns.tour_session.id)
     {:ok, tour_stop} = TourStops.create_tour_stop(create_params)
@@ -74,6 +84,7 @@ defmodule TourinItWeb.Organize.Components.TourStopsLive do
             <div>{tour_stop.end_date}</div>
             <div>
               <button type="button">Edit</button>
+              <button class="ml-2" type="button" phx-click="delete" phx-value-id={tour_stop.id} phx-target={@myself}>Delete</button>
             </div>
           </div>
           <.tour_stop_form :if={editing?(tour_stop)} changeset={changeset(@tour_stop)} myself={@myself} />
