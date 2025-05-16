e6f275c867a0b55afea176e45f5c1819aaa98cfa
Joel Lubrano <joel.lubrano@gmail.com>
2025-05-16 00:09:43 -0400

    Configure tz database

--- mix.exs
+++ mix.exs
@@ -57,7 +57,8 @@ defmodule TourinIt.MixProject do
       {:gettext, "~> 0.26"},
       {:jason, "~> 1.2"},
       {:dns_cluster, "~> 0.1.1"},
-      {:bandit, "~> 1.5"}
+      {:bandit, "~> 1.5"},
+      {:tz, "~> 0.28"}
     ]
   end
 
