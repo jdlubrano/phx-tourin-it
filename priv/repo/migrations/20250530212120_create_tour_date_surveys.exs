9cde146bbc5018157ebe3c6293d16e4e6aab75ed
Joel Lubrano <joel.lubrano@gmail.com>
2025-05-31 22:33:05 -0400

    Add unique index to tour_date_surveys

--- priv/repo/migrations/20250530212120_create_tour_date_surveys.exs
+++ priv/repo/migrations/20250530212120_create_tour_date_surveys.exs
@@ -4,6 +4,8 @@ defmodule TourinIt.Repo.Migrations.CreateTourDateSurveys do
   def change do
     create table(:tour_date_surveys) do
       add :availability, :string
+      add :note, :string
+
       add :tour_date_id, references(:tour_dates, on_delete: :delete_all)
       add :tour_goer_id, references(:tour_goers, on_delete: :delete_all)
 
@@ -11,6 +13,6 @@ defmodule TourinIt.Repo.Migrations.CreateTourDateSurveys do
     end
 
     create index(:tour_date_surveys, [:tour_date_id])
-    create index(:tour_date_surveys, [:tour_goer_id])
+    create unique_index(:tour_date_surveys, [:tour_goer_id, :tour_date_id])
   end
 end
