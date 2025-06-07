fe055312b26858cf3ce6a4c17838b4c6f91812e8
Joel Lubrano <joel.lubrano@gmail.com>
2025-06-07 10:24:23 -0400

    Rewrite PageController to redirect to tour_session upcoming page

--- lib/tourin_it_web/controllers/page_controller.ex
+++ lib/tourin_it_web/controllers/page_controller.ex
@@ -1,9 +1,23 @@
 defmodule TourinItWeb.PageController do
   use TourinItWeb, :controller
 
+  alias TourinIt.Organize
+  alias TourinIt.Organize.TourSession
+
+  plug :redirect_to_upcoming_tour_date
+
   def home(conn, _params) do
-    # The home page is often custom made,
-    # so skip the default app layout.
-    render(conn, :home, layout: false)
+    render(conn, :home)
+  end
+
+  defp redirect_to_upcoming_tour_date(conn, _options) do
+    case Organize.default_tour_session(conn.assigns.current_user) do
+      %TourSession{} = tour_session ->
+        conn
+        |> redirect(to: ~p"/tours/#{tour_session.tour.slug}/#{tour_session.identifier}/upcoming")
+        |> halt()
+
+      _ -> conn
+    end
   end
 end
