d0a943925bf7a2d51824e0c1977c0b94ef83b788
Joel Lubrano <joel.lubrano@gmail.com>
2025-06-12 00:05:08 -0400

    Consolidate similar ensure_invited! functions

--- lib/tourin_it_web/access/tour_goer.ex
+++ lib/tourin_it_web/access/tour_goer.ex
@@ -9,13 +9,12 @@ defmodule TourinItWeb.Access.TourGoer do
 
   alias TourinItWeb.UserNotInvitedError
 
-  def ensure_invited!(%User{} = current_user, %TourSession{} = tour_session) do
-    unless TourGoers.invited?(current_user.id, tour_session.id) || Accounts.admin?(current_user) do
-      raise UserNotInvitedError, "User ##{current_user.id} not invited to TourSession ##{tour_session.id}"
+  def ensure_invited!(%User{} = current_user, tour_session) do
+    tour_session_id = case tour_session do
+      %TourSession{id: id} -> id
+      id when is_integer(id) -> id
     end
-  end
 
-  def ensure_invited!(%User{} = current_user, tour_session_id) do
     unless TourGoers.invited?(current_user.id, tour_session_id) || Accounts.admin?(current_user) do
       raise UserNotInvitedError, "User ##{current_user.id} not invited to TourSession ##{tour_session_id}"
     end
