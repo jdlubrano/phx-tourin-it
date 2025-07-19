a1a433c33a50d73d0ed432c8759756901ec36cf7
Joel Lubrano <joel.lubrano@gmail.com>
2025-07-19 18:25:55 -0400

    Bump to v0.3.2

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule TourinIt.MixProject do
   def project do
     [
       app: :tourin_it,
-      version: "0.3.1",
+      version: "0.3.2",
       elixir: "~> 1.14",
       elixirc_paths: elixirc_paths(Mix.env()),
       start_permanent: Mix.env() == :prod,
