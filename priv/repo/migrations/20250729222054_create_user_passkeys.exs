c04761a68d6e7609c2446a8f386e5861ee1ebf64
Joel Lubrano <joel.lubrano@gmail.com>
2025-07-29 19:29:56 -0400

    Fixup migration

--- priv/repo/migrations/20250729222054_create_user_passkeys.exs
+++ priv/repo/migrations/20250729222054_create_user_passkeys.exs
@@ -4,12 +4,12 @@ defmodule TourinIt.Repo.Migrations.CreateUserPasskeys do
   def change do
     create table(:user_passkeys) do
       add :credential_id, :binary, null: false
-      add :public_key, :binary, null: false
+      add :public_key_binary, :binary, null: false
       add :user_id, references(:users, on_delete: :delete_all), null: false
 
       timestamps(type: :utc_datetime)
     end
 
-    create index(:user_passkeys, [:user_id])
+    create(unique_index(:user_passkeys, [:user_id, :credential_id]))
   end
 end
