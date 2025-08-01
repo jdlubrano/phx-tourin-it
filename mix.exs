d88230a8293cbac083ebe3117e272cd97f7d35da
Joel Lubrano <joel.lubrano@gmail.com>
2025-07-31 20:31:07 -0400

    v0.4.2

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule TourinIt.MixProject do
   def project do
     [
       app: :tourin_it,
-      version: "0.4.1",
+      version: "0.4.2",
       elixir: "~> 1.17",
       elixirc_paths: elixirc_paths(Mix.env()),
       start_permanent: Mix.env() == :prod,
